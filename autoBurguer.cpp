53fd42af88baecc0ea043af06023d75e5c5a4170
maudavtej <tejeromauricio80@gmail.com>
2026-03-14 00:31:57 -0600

    funcion auto-burguer terminada..

--- autoBurguer.cpp
+++ autoBurguer.cpp
@@ -18,7 +18,11 @@ struct autoPack
     caract descr;
 };
 
-
+void limpiarPantalla()
+{
+    // Código ANSI: \033[2J borra pantalla, \033[H mueve cursor al inicio
+    cout << "\033[2J\033[H";
+}
 float menu(char historial[]);
 int genNOrden()
 {
@@ -29,98 +33,90 @@ int validacion(int min, int max)
     int valor;
     while (scanf("%d", &valor) != 1 || valor < min || valor > max)
     {
-        printf("Opción inválida. Intente nuevamente: ");
+        printf("Opcion invalida. Intente nuevamente: ");
         while (getchar() != '\n')
             ;
     }
     return valor;
 }
 
-float menu(char historial[]){
-    autoPack paquetes[3]={
-        {1, "ADULTO", 249, "Hamburguesa doble + hamburguesa sencilla\nPapas grandes\nRefresco grande"},
-        {2, "NIÑO", 149,"Hamburguesa sencilla\nPapas chicas\nRefresco chico\n-Nuggets"},
-        {3, "FAMILIAR", 499, "4 hamburguesas con queso\n4 refrescos grandes\n4 papas grandes, 4 helados"}
-    };
-    autoPack *apunt = paquetes;
-int eleccion, eleccionMen;
-float total=0;
-do
+void procesarCompra(autoPack paquete, int *contadorTotal, float *acumuladorDinero, char historial[])
 {
-    cout<< "Bienvenido a auto-burguer\n Seleccione una opcion:\n1.Paquete adulto\n2.Paquete niño\n3.Paquete familiar\n4.Pagar\n";
-    eleccion = validacion(1,3);
-    switch (eleccion)
+    int eleccionMen, cantidad;
+    cout << "\nPaquete: " << paquete.caracteristicas << "- $" << paquete.precio << endl;
+    cout << "\nDescripcion:\n"
+         << paquete.descr.descripcion << endl;
+    cout << "1.Continuar | 2.Regresar" << endl;
+    eleccionMen = validacion(1, 2);
+    if (eleccionMen == 2)
     {
-    case 1:
-        cout<<"Paquete:"<<apunt[0].caracteristicas<<"\n";
-        cout<<"$"<<apunt[0].precio<<"\n";
-        cout<<"\nDescripcion\n"<<apunt[0].descr.descripcion<<"\n";
-        
-
-        cout<<"\n1.Continuar con la compra\n2.regresar al menu\n";
-        eleccionMen=validacion(1,2);
-        if (eleccionMen==2)
-        {
-            cout<<"regresando al menu principal...\n";
-            break;
-        }else{
-            cout<<"ingrese el numero de paquetes que desea agregar: \n";
-            eleccion = validacion(1,10);
-            strcat(historial, apunt[0].caracteristicas);
-            strcat(historial, "\n");
-            total+= (apunt[0].precio*eleccion);
-            break;
-            }
-    case 2:
-        cout<<"Paquete:\n"<<apunt[1].caracteristicas<<"\n";
-        cout<<"$"<<apunt[1].precio<<"\n";
-        cout<<"\nDescripcion\n"<<apunt[1].descr.descripcion;
-
-
-        cout<<"\n1.Continuar con la compra\n2.regresar al menu\n";
-        eleccionMen=validacion(1,2);
-        if (eleccionMen==2)
-        {
-            cout<<"regresando al menu principal...\n";
-            break;
-        }else{
-            cout<<"ingrese el numero de paquetes que desea agregar: \n";
-            eleccion = validacion(1,10);
-            strcat(historial, apunt[1].caracteristicas);
-            strcat(historial, "\n");
-            total+= (apunt[1].precio*eleccion);
-            break;
-            }
-    case 3:
-    cout<<apunt[2].caracteristicas<<"\n";
-    cout<<"$"<<apunt[2].precio<<"\n";
-    cout<<"\nDescripcion\n"<<apunt[2].descr.descripcion;
-
-
-    cout<<"\n1.Continuar con la compra\n2.regresar al menu\n";
-    eleccionMen=validacion(1,2);
-    if (eleccionMen==3)
-    {
-        cout<<"regresando al menu principal...\n";
-        break;
-    }else{
-        cout<<"ingrese el numero de paquetes que desea agregar: \n";
-        eleccion = validacion(1,10);
-        strcat(historial, apunt[2].caracteristicas);
-        strcat(historial, "\n");
-        total+= (apunt[2].precio*eleccion);
-        break;
-        }
-    default:
-        break;
+        cout << "Regresando al menu...";
     }
-} while (eleccion!=4);
-return total;
-
+    else
+    {
+        cout << "Cantidad de paquetes que desea agregar:";
+        cantidad = validacion(1, 10);
+        *contadorTotal += cantidad;
+        *acumuladorDinero += (paquete.precio * cantidad);
+        strcat(historial, paquete.caracteristicas);
+        strcat(historial, "\n");
+        cout << "AGREGADO CORRECTAMENTE\n";
+        cout << "Presione enter para continuar.\n";
+        getchar();
+        getchar();
+    }
 }
-int main(){
+
+float menu(char historial[])
+{
+    autoPack paquetes[3] = {
+        {1, "ADULTO", 249, {"Hamburguesa doble + hamburguesa sencilla\nPapas grandes\nRefresco grande"}},
+        {2, "NINIO", 149, {"Hamburguesa sencilla\nPapas chicas\nRefresco chico\nNuggets"}},
+        {3, "FAMILIAR", 499, {"4 hamburguesas con queso\n4 refrescos grandes\n4 papas grandes, 4 helados"}}};
+    autoPack *apunt = paquetes;
+    int eleccion, contador = 0;
+    float total = 0;
+    do
+    {
+        limpiarPantalla();
+        cout << "==== Bienvenido a auto-burguer ====\n Seleccione una opcion:\n1.Paquete adulto ($249)\n2.Paquete ninio ($149)\n3.Paquete familiar ($499)\n4.Pagar\n";
+        eleccion = validacion(1, 4);
+        switch (eleccion)
+        {
+        case 1:
+            procesarCompra(apunt[0], &contador, &total, historial);
+            break;
+        case 2:
+            procesarCompra(apunt[1], &contador, &total, historial);
+            break;
+
+        case 3:
+            procesarCompra(apunt[2], &contador, &total, historial);
+            break;
+
+        default:
+
+            break;
+        }
+    } while (eleccion != 4);
+    return total;
+}
+int main()
+{
     srand(time(NULL));
-    char historial[100]= "";
+    char historial[1000] = "";
     float total = menu(historial);
     int folio = genNOrden();
+    if (total > 0)
+    {
+        cout << "\n ==== TICKET ====" << endl;
+        cout << "ORDEN:" << folio << endl;
+        cout << "PAQUETES SELECCIONADOS: \n"
+             << historial << endl;
+        cout << "TOTAL A PAGAR:$" << total << endl;
+    }
+    else
+    {
+        cout << "No se agregaron articulos.";
+    }
 }
